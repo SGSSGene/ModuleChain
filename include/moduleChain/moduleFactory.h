a9d1bc3673777438dd4fe1edd1c6bd1930d0cc43
Simon Gene Gottlieb <gene@staubsaal.de>
2015-02-08 00:49:09 +0100

    adding error messages

--- include/moduleChain/moduleFactory.h
+++ include/moduleChain/moduleFactory.h
@@ -13,10 +13,19 @@ public:
 	ModuleFactory(std::string const& _name, std::function<void(Chain&)> _func) {
 		registerModule(_name, _func);
 	}
+
 	static void registerModule(std::string const& _name, std::function<void(Chain&)> _func) {
+		if (getModuleMap().find(_name) != getModuleMap().end()) {
+			std::cerr<<"moduleChain: overriding module register for "<<_name<<std::endl;
+		}
 		getModuleMap()[_name] = _func;
 	}
+
 	static void createInstance(Chain& _Chain, std::string const& _name) {
+		if (getModuleMap().find(_name) == getModuleMap().end()) {
+			std::cerr<<"moduleChain: Can't create instance of module, not registered: "<<_name<<std::endl;
+			return;
+		}
 		getModuleMap().at(_name)(_Chain);
 	}
 };
