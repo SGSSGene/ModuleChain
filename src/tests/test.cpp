966aa8caa1e96bdcde988ca7fa3739b7b87a7ce2
Simon Gene Gottlieb <gene@staubsaal.de>
2015-02-08 14:41:04 +0100

    improving test.cpp

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -22,14 +22,18 @@ public:
 		std::cout<<"init, only called once"<<std::endl;
 	}
 	void execute() {
-		std::cout<<"T1"<<std::endl;
+		std::cout<<"T1: start"<<std::endl;
+		sleep(1);
+		std::cout<<"T1: end"<<std::endl;
 	}
 };
 class T3 {
 	moduleChain::Provide<R1> r1;
 public:
 	void execute() {
-		std::cout<<"T3"<<std::endl;
+		std::cout<<"T3: start"<<std::endl;
+		sleep(1);
+		std::cout<<"T3: end"<<std::endl;
 	}
 
 };
@@ -37,18 +41,19 @@ class T2 {
 	moduleChain::Require<R2> r2;
 public:
 	void execute() {
-		std::cout<<"start T2"<<std::endl;
+		std::cout<<"T2: start"<<std::endl;
 		sleep(1);
-		std::cout<<"end   T2"<<std::endl;
+		std::cout<<"T2: end"<<std::endl;
 	}
 };
 class T4 {
-	moduleChain::Require<R2> r2;
+	moduleChain::Require<R1> r1;
+	moduleChain::Provide<R2> r2;
 public:
 	void execute() {
-		std::cout<<"start T4"<<std::endl;
+		std::cout<<"T4: start"<<std::endl;
 		sleep(1);
-		std::cout<<"end   T4"<<std::endl;
+		std::cout<<"T4: end"<<std::endl;
 	}
 };
 
@@ -61,7 +66,7 @@ REGISTER_MODULE(T4);
 int main() {
 	moduleChain::Chain l1("test", {"T1", "T2", "T3", "T4"}, 2);
 
-	for (int i(0); i<10; ++i) {
+	for (int i(0); i<3; ++i) {
 		std::cout<<std::endl<<"loop: "<<i<<std::endl;
 		l1.run();
 	}
